45f212d5556068bedc4126706dd915dd77bf5ed2
bishal5917 <poudelb172@gmail.com>
2023-10-22 00:37:28 +0545

    login activity

---
+++ app/src/main/java/com/example/geetsunam/features/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.geetsunam.features.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.geetsunam.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
