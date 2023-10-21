547fd8b9cf55152d16e7bf2ccfb1537b7115400b
bishal5917 <poudelb172@gmail.com>
2023-10-21 13:38:22 +0545

    gradle setup,dependencies added

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+//    ext {
+//        val hilt_version = "2.44"
+//        val kotlin_version = "1.8.0"
+//    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
