5803ebbaf3783dbea7ddd99e39d58c6ae81d7831
bishal5917 <poudelb172@gmail.com>
2023-11-07 12:40:03 +0545

    drawer and bottom view handled

--- app/src/main/java/com/example/geetsunam/MainActivity.kt
+++ app/src/main/java/com/example/geetsunam/MainActivity.kt
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageButton
-import android.window.OnBackInvokedDispatcher
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.example.geetsunam.features.presentation.home.HomeFragment
 import com.example.geetsunam.utils.CustomToast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -38,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         val drawerBtn = findViewById<ImageButton>(R.id.ibMenu)
         drawerLayout = findViewById<DrawerLayout>(R.id.dlAppDrawer)
         val navigationView = findViewById<NavigationView>(R.id.drawerNavView)
+        navigationView.setupWithNavController(navController)
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, R.string.app_name, R.string.app_name
         )
@@ -52,24 +48,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.navHostFragment, HomeFragment())
-                .commit()
-            navigationView.setCheckedItem(R.id.homeFragment)
+        navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_logout -> {
+                    drawerLayout.closeDrawer(navigationView)
+                    CustomToast.showToast(this, "Logged out")
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        super.onBackPressed()
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            onBackPressedDispatcher.onBackPressed()
-        }
-    }
+//    @Deprecated("Deprecated in Java")
+//    override fun onBackPressed() {
+//        super.onBackPressed()
+//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            drawerLayout.closeDrawer(GravityCompat.START)
+//        } else {
+//            onBackPressedDispatcher.onBackPressed()
+//        }
+//    }
 }
